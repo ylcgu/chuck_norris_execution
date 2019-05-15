d775ca5ccaadd37278e3e992c5539c095a3962ab
Austin Lasseter <31504432+austinlasseter@users.noreply.github.com>
2019-05-15 15:31:23 -0400

    Superman wears Chuck Norris pajamas

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+######### Import your libraries #######
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import os
 
+
+####### Set up your app #####
 app = dash.Dash(__name__)
 server = app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+
+####### Layout of the app ########
 app.layout = html.Div([
     html.H2('Chuck Norris execution method'),
     dcc.Dropdown(
@@ -18,10 +23,14 @@ app.layout = html.Div([
     html.Div(id='display-value')
 ])
 
+
+######### Interactive callbacks go here #########
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(value):
     return 'Chuck Norris will execute by "{}"'.format(value)
 
+
+######### Run the app #########
 if __name__ == '__main__':
     app.run_server(debug=True)
